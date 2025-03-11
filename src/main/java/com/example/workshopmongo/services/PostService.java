52c72f97db70824089bde4ce13090282bca7cbfa
Erick Daniel <derickanielgg@gmail.com>
2025-03-10 22:40:16 -0300

    Consulta simples com query methods

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.example.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,10 @@ public class PostService {
 	    	throw new ObjectNotFoundException("Objeto não encontrado");
 	    }
 	    return user;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
